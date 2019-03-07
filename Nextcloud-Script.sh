4a64cfaef9877da8e13dc9aa97c955386d1634ba
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-07 10:15:34 +0100

    Update Nextcloud-Script.sh

--- Nextcloud-Script.sh
+++ Nextcloud-Script.sh
@@ -46,3 +46,9 @@ echo "$SECURE_MYSQL"
 
 apt -y purge expect
 apt autoremove -y
+
+    echo "Please enter root user MySQL password!"
+    read rootpasswd
+    mysql -uroot -p${rootpasswd} -e "CREATE DATABASE nextcloud;"
+    mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud_user'@'localhost' IDENTIFIED BY 'PASSWORD';"
+    mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
