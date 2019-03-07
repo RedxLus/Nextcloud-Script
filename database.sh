d59c912320552adec6a43fe155fb6c22afe5533c
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-07 10:13:35 +0100

    Update database.sh

--- database.sh
+++ database.sh
@@ -5,4 +5,3 @@
     mysql -uroot -p${rootpasswd} -e "CREATE DATABASE nextcloud;"
     mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud_user'@'localhost' IDENTIFIED BY 'PASSWORD';"
     mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
-    mysql -uroot -p${rootpasswd} -e "exit;"
