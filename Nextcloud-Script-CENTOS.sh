0ecdf2518be7e9ce8271cfca090e536320eec5c8
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-07 22:31:31 +0100

    Update Nextcloud-Script-CENTOS.sh

--- Nextcloud-Script-CENTOS.sh
+++ Nextcloud-Script-CENTOS.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+ 
+yum update -y
+yum install epel-release -y
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+
+yum install httpd php70w php70w-dom php70w-mbstring php70w-gd php70w-pdo php70w-json php70w-xml php70w-zip php70w-curl php70w-mcrypt php70w-pear setroubleshoot-server bzip2
+
+yum install mariadb-server php70w-mysql
+
+systemctl start mariadb
+systemctl enable mariadb
+
 
