39a6705645d53c39ce2ddf68a2ee70d97c374ef3
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-07 12:57:57 +0100

    Update Nextcloud-Script.sh

--- Nextcloud-Script.sh
+++ Nextcloud-Script.sh
@@ -4,7 +4,8 @@ apt update && apt upgrade -y
  
 apt install apache2 -y && systemctl start apache2 && systemctl enable apache2
  
-apt install php7.0 libapache2-mod-php7.0 php7.0-common php7.0-gd php7.0-json php7.0-mysql php7.0-curl php7.0-mbstring php7.0-intl php7.0-mcrypt php7.0-imagick php7.0-xml php7.0-zip unzip curl aria2 -y
+apt install php7.0 libapache2-mod-php7.0 php7.0-common php7.0-gd php7.0-json php7.0-mysql php7.0-curl php7.0-mbstring php7.0-intl php7.0-mcrypt php7.0-imagick php7.0-xml php7.0-zip -y
+apt install unzip curl aria2 -y
  
 apt -y install mariadb-server
  
