98c4e13f695a3bb479007d0832ed3a64b1a8ec3c
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-07 22:58:05 +0100

    Update Nextcloud-Script-DEBIAN.sh

--- Nextcloud-Script-DEBIAN.sh
+++ Nextcloud-Script-DEBIAN.sh
@@ -2,7 +2,7 @@
  
 apt update && apt upgrade -y
  
-apt install apache2 -y && systemctl start apache2 && systemctl enable apache2 && a2enmod rewrite headers env dir mime && systemctl restart apache2
+apt install apache2 -y && systemctl start apache2 && systemctl enable apache2 && a2enmod rewrite headers env dir mime && a2enmod ssl && systemctl restart apache2
  
 apt install php7.0 libapache2-mod-php7.0 php7.0-common php7.0-gd php7.0-json php7.0-mysql php7.0-curl php7.0-mbstring php7.0-intl php7.0-mcrypt php7.0-imagick php7.0-xml php7.0-zip -y
 apt install unzip curl aria2 -y
