cd2e3fe7c1d36e0b8788a42adce2187933262449
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-07 21:17:01 +0100

    Update Nextcloud-Script-UBUNTU-16.sh

--- Nextcloud-Script-UBUNTU-16.sh
+++ Nextcloud-Script-UBUNTU-16.sh
@@ -2,21 +2,55 @@
 
 apt update && apt upgrade -y
  
- apt-get install apache2 libapache2-mod-php7.0 bzip2 -y
+ apt-get install apache2 libapache2-mod-php7.0 bzip2 unzip curl -y
  apt-get install php7.0-gd php7.0-json php7.0-mysql php7.0-curl php7.0-mbstring -y
  apt-get install php7.0-intl php7.0-mcrypt php-imagick php7.0-xml php7.0-zip -y
  
  apt-get install mariadb-server php-mysql -y
  
- mysql -u root -p
- 
- CREATE DATABASE nextcloud;
- CREATE USER 'admin'@'localhost' IDENTIFIED BY 'Contraseña';
- GRANT ALL PRIVILEGES ON nextcloud.* TO 'admin'@'localhost';
- FLUSH PRIVILEGES;
- exit;
+ apt -y install expect
 
-# cd /var/www
+// Not required in actual script
+MYSQL_ROOT_PASSWORD=abcd1234
+
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"Enter current password for root (enter for none):\"
+send \"$MYSQL\r\"
+expect \"Change the root password?\"
+send \"n\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+
+echo "$SECURE_MYSQL"
+
+apt -y purge expect
+apt autoremove -y
+
+
+    echo ""
+    echo "Please enter root user MySQL password!"
+    read rootpasswd
+    mysql -uroot -p${rootpasswd} -e "CREATE DATABASE nextcloud;"
+    mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON nextcloud.* TO 'admin'@'localhost' IDENTIFIED BY '$rootpasswd';"
+    mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+
+
+curl -LO https://download.nextcloud.com/server/releases/nextcloud-13.0.1.zip
+
+unzip nextcloud-13.0.1.zip -d /var/www/html/
+chown -R www-data:www-data /var/www/html/nextcloud/
+
+# cd /var/www/html
 # wget https://download.nextcloud.com/server/releases/latest-13.tar.bz2 -O nextcloud-13-latest.tar.bz2
 # tar -xvjf nextcloud-13-latest.tar.bz2
 # chown -R www-data:www-data nextcloud
