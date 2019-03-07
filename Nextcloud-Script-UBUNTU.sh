42983661c9d6716bfdb9084be2982cf9992522a7
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-07 14:07:06 +0100

    Update Nextcloud-Script-UBUNTU.sh

--- Nextcloud-Script-UBUNTU.sh
+++ Nextcloud-Script-UBUNTU.sh
@@ -3,8 +3,10 @@
 apt update && apt upgrade -y
  
 apt install apache2 -y && systemctl start apache2 && systemctl enable apache2
+
+sudo apt-get install software-properties-common -y
  
-apt install php7.0 libapache2-mod-php7.0 php7.0-common php7.0-gd php7.0-json php7.0-mysql php7.0-curl php7.0-mbstring php7.0-intl php7.0-mcrypt php7.0-imagick php7.0-xml php7.0-zip -y
+apt install libapache2-mod-php7.0 bzip2 php7.0-gd php7.0-json php7.0-mysql php7.0-curl php7.0-mbstring php7.0-intl php7.0-mcrypt php-imagick php7.0-xml php7.0-zip php-mysql -y
 apt install unzip curl aria2 -y
  
 apt -y install mariadb-server
