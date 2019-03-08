5aad5b436c3f5a86b37183db0e297fed67f2477b
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-08 23:21:00 +0100

    Update menu.sh

--- Archivos/menu.sh
+++ Archivos/menu.sh
@@ -2,9 +2,9 @@
 clear
 echo "¿Cual es tu sistema operativo?" 
 echo ""
-echo "1. Tal"
-echo "2. " 
-echo "3. " 
+echo "1. UBUNTU 16"
+echo "2. UBUNTU 18" 
+echo "3. DEBIAN" 
 echo -n "Seleccione una opcion [1 - 3]"
   read x
   case $x in
