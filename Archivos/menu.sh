ce6e8fcbfc4ffe50f7451b412441a9f9309c7d40
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-08 23:32:47 +0100

    Update menu.sh

--- Archivos/menu.sh
+++ Archivos/menu.sh
@@ -11,15 +11,17 @@ echo -n "Seleccione una opcion [1 - 3]"
      1)
         echo "Descargando y ejecutando Script para UBUNTU 16"
         wget https://raw.githubusercontent.com/RedxLus/Nextcloud-Script/master/Nextcloud-Script-UBUNTU-16.sh --no-check-certificate
+        sh Nextcloud-Script-UBUNTU-16.sh
      ;;
      2)
         echo "Descargando y ejecutando Script para UBUNTU 18"
-       wget https://raw.githubusercontent.com/RedxLus/Nextcloud-Script/master/Nextcloud-Script-UBUNTU-18.sh --no-check-certificate
+        wget https://raw.githubusercontent.com/RedxLus/Nextcloud-Script/master/Nextcloud-Script-UBUNTU-18.sh --no-check-certificate
+        sh Nextcloud-Script-UBUNTU-18.sh
      ;;
      3)
-       echo "Descargando y ejecutando Script para DEBIAN"
-       wget https://raw.githubusercontent.com/RedxLus/Nextcloud-Script/master/Nextcloud-Script-DEBIAN.sh --no-check-certificate
-       
+        echo "Descargando y ejecutando Script para DEBIAN"
+        wget https://raw.githubusercontent.com/RedxLus/Nextcloud-Script/master/Nextcloud-Script-DEBIAN.sh --no-check-certificate
+        sh Nextcloud-Script-DEBIAN.sh
      ;;
      *)
         echo "no se qué numero es"
