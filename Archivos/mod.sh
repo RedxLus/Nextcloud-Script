91260f810ab00b566d96f4a3de038ed36148a508
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-09 13:38:58 +0100

    Update mod.sh

--- Archivos/mod.sh
+++ Archivos/mod.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-laip=$(sudo ifconfig|awk 'NR == 2'|awk '{print $2}'|cut -d ':' -f2)
+apt-get install net-tools
+laip=$(ifconfig|awk 'NR == 2'|awk '{print $2}'|cut -d ':' -f2)
 
 echo "Su IP es $laip. Vamos a añadirla como Dominio de Confianza:"
 
-sed -i '1i &laip' archivo.txt
+sed -i "8i 0 => \'&laip\',"  config.php
 
+echo "Añadida"
