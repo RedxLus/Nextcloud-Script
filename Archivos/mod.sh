13e89776c65d97a843e620c5814fec739611b6b6
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-09 13:40:22 +0100

    Update mod.sh

--- Archivos/mod.sh
+++ Archivos/mod.sh
@@ -5,6 +5,6 @@ laip=$(ifconfig|awk 'NR == 2'|awk '{print $2}'|cut -d ':' -f2)
 
 echo "Su IP es $laip. Vamos a añadirla como Dominio de Confianza:"
 
-sed -i "8i 0 => \'&laip\',"  config.php
+sed -i "8i 1 => \'&laip\',"  config.php
 
 echo "Añadida"
