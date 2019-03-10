b21e21247a0762da51ba29e2fb2c461640033d25
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-10 18:02:06 +0100

    Update menu.sh

--- Archivos/menu.sh
+++ Archivos/menu.sh
@@ -20,7 +20,8 @@ echo ""
 echo "1. UBUNTU 16"
 echo "2. UBUNTU 18" 
 echo "3. DEBIAN" 
-echo -n "Seleccione una opcion [1 - 3]"
+echo "4. CentOS" 
+echo -n "Seleccione una opcion [1 - 4]"
   read seleccion
   case $seleccion in
      1)
@@ -40,6 +41,12 @@ echo -n "Seleccione una opcion [1 - 3]"
         wget https://raw.githubusercontent.com/RedxLus/Nextcloud-Script/master/Nextcloud-Script-DEBIAN.sh --no-check-certificate
         chmod +x Nextcloud-Script-DEBIAN.sh
         sh Nextcloud-Script-DEBIAN.sh && rm -r Nextcloud-Script-DEBIAN.sh
+     ;;
+     4)
+        echo "Descargando y ejecutando Script para DEBIAN"
+        curl -LO https://raw.githubusercontent.com/RedxLus/Nextcloud-Script/master/Nextcloud-Script-CENTOS.sh -k
+        chmod +x Nextcloud-Script-CENTOS.sh
+        sh Nextcloud-Script-CENTOS.sh && rm -r Nextcloud-Script-CENTOS.sh
      ;;
      *)
         echo "Numero no reconocido."
