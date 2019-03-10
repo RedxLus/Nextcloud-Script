0dee67c4fce08e7a2b5adf6d96a5a77e1555c908
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-10 18:25:12 +0100

    Create pedir.sh

---
+++ Archivos/pedir.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo ""
+echo "¿Desea Instalar OcDownloader (plugin para Nextcloud)?"
+echo "1. Si. Me gustaria la instalacion automatica de OcDownloader." 
+echo "2. No. Gracias. Salir" 
+echo -n "Seleccione una opcion [1 - 2]"
+  read seleccion
+  
+  case $seleccion in
+     1)
+        echo "Instalando y activando"
+        curl -LO https://raw.githubusercontent.com/RedxLus/Nextcloud-Script/master/ocDownloader.sh -k
+        chmod +x ocDownloader.sh
+        sh ocDownloader.sh && rm -r ocDownloader.sh
+     ;;
+     2)
+        echo "Saliendo"
+     ;;
+     *)
+        echo "Numero no reconocido."
+     ;;
+  esac
