702f92f0bcd8e8141f62b035e1fcade5c68c55db
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-10 22:31:23 +0100

    Update mod.sh

--- Archivos/mod.sh
+++ Archivos/mod.sh
@@ -3,6 +3,8 @@
 apt-get install net-tools
 laip=$(ifconfig|awk 'NR == 2'|awk '{print $2}'|cut -d ':' -f2)
 
+clear
+
 echo "Su IP es $laip. ¿Es correcta?. Puede modificarla despues en /var/www/html/nextcloud/config/config.php "
 echo "1. Si. Puede añadirla automaticamente para que funcione NextCloud."
 echo "2. No. No es mi ip o quiero hacerlo manual."
