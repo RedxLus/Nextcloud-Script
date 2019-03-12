1f7ebd9580d59f99a2ca6a6eb238939f335feb4f
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-12 22:29:34 +0100

    Update Nextcloud-Script-UBUNTU-18.sh

--- Archivos-so/Nextcloud-Script-UBUNTU-18.sh
+++ Archivos-so/Nextcloud-Script-UBUNTU-18.sh
@@ -62,6 +62,9 @@ systemctl restart apache2
 
 cd /var/www/html/nextcloud && sudo -u www-data php occ  maintenance:install --database "mysql" --database-name "nextcloud"  --database-user "admin" --database-pass "$rootpasswd" --admin-user "admin" --admin-pass "$rootpasswd" 
 
+cd /var/www/html/nextcloud && sed -i "1i <IfModule mod_rewrite.c>" .htaccess && sed -i "2i RewriteCond %{HTTPS} off" .htaccess && sed -i "3i RewriteRule (.*) https://%{HTTP_HOST}%{REQUEST_URI} [R=301,L]" .htaccess && sed -i "4i </IfModule>" .htaccess
+systemctl restart apache2
+
 curl -LO https://raw.githubusercontent.com/RedxLus/Nextcloud-Script/master/Archivos/ip-config.php.sh -k && sh ip-config.php.sh && rm ip-config.php.sh
 
 curl -LO https://raw.githubusercontent.com/RedxLus/Nextcloud-Script/master/Archivos/instalar-oc.sh -k && sh instalar-oc.sh && rm instalar-oc.sh
