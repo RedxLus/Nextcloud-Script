e39c7c55f079a01691d7be574e7c987f61db57a0
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-13 12:35:25 +0100

    Update Optimization-3.sh

--- Opt/Optimization-3.sh
+++ Opt/Optimization-3.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 apt install redis-server php-redis -y
-sed -i "19i -- " /var/www/html/nextcloud/config/config.php
-sed -i "20i 'memcache.locking' => '\\\\OC\\\\Memcache\\\\Redis'," /var/www/html/nextcloud/config/config.php
+sed -i "19i 'memcache.distributed' => '\\\\OC\\\\Memcache\\\\Redis', " /var/www/html/nextcloud/config/config.php
+sed -i "20i 'redis' => [ " /var/www/html/nextcloud/config/config.php
+sed -i "21i      'host' => 'redis-host.example.com', " /var/www/html/nextcloud/config/config.php
+sed -i "22i      'port' => 6379, " /var/www/html/nextcloud/config/config.php
+sed -i "23i ], " /var/www/html/nextcloud/config/config.php
+
+sed -i "24i 'memcache.locking' => '\\\\OC\\\\Memcache\\\\Redis'," /var/www/html/nextcloud/config/config.php
