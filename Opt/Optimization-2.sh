c6353dad220cfdbb793240a3f76703e0a9a735ba
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-13 12:40:59 +0100

    Update Optimization-2.sh

--- Opt/Optimization-2.sh
+++ Opt/Optimization-2.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-memcache=$('memcache.local' => '\OC\Memcache\APCu',)
 
 apt install php-apcu -y
 sed -i "18i 'memcache.local' => '\\\\OC\\\\Memcache\\\\APCu'," /var/www/html/nextcloud/config/config.php
+
+service apache2 restart
 
 #/etc/php/7.0/apache2
 #apc.enable_cli to 1 on your php.ini
