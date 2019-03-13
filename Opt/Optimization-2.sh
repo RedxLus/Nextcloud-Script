f9276a93cf502b276ede6c973ae5e5995410c8dd
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-13 12:27:13 +0100

    Update Optimization-2.sh

--- Opt/Optimization-2.sh
+++ Opt/Optimization-2.sh
@@ -2,9 +2,7 @@
 memcache=$('memcache.local' => '\OC\Memcache\APCu',)
 
 apt install php-apcu
-sed -i "18i 'memcache.local' => '\OC\Memcache\APCu',"  config.php
+sed -i "18i 'memcache.local' => '\\\\OC\\\\Memcache\\\\APCu',"  config.php
 
-
-
-
-apc.enable_cli to 1 on your php.ini
+#/etc/php/7.0/apache2
+#apc.enable_cli to 1 on your php.ini
